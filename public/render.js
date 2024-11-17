9fc769c817150b0a42708497477eff35a4fd4f4b
kapiso-TW <000617jason@gmail.com>
2024-11-17 23:23:36 +0800

    IDK

--- public/render.js
+++ public/render.js
@@ -59,7 +59,7 @@ function addMessage(msg) {
         const deleteButton = document.createElement('button');
         deleteButton.className = 'button';
         deleteButton.style = 'display: flex; align-items: center;';
-        deleteButton.innerHTML = <svg viewBox="0 0 448 512" class="svgIcon" style="width: 20px; height: 20px; fill: currentColor;"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>;
+        deleteButton.innerHTML = '<svg viewBox="0 0 448 512" class="svgIcon" style="width: 20px; height: 20px; fill: currentColor;"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>';
         deleteButton.onclick = () => retractMessage(msg.id); // 綁定收回事件
         messageWrapper.appendChild(deleteButton);
     }
